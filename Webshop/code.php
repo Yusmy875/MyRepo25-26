f62768e26f086fbbace4cf6a05f41720aa913f0f
Mikhailo <mikhailo@com>
2025-10-07 12:54:05 +0200

    à

---
+++ Webshop/code.php
@@ -0,0 +1,16 @@
+<?php 
+function renderNavbar() {
+?>
+<div class="navbar">
+    <a href="index.php">Products</a>
+    <a href="about.php">About</a>
+    <a href="contact.php">Contact</a>
+</div>
+
+
+
+<?php
+}
+
+
+?>
