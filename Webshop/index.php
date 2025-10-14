a9c9e2bcee5a15c504692b397d115d5c859aa65b
Mikhailo <mikhailo@com>
2025-10-14 13:40:30 +0200

    qwcdfrf

--- Webshop/index.php
+++ Webshop/index.php
@@ -14,19 +14,19 @@
 // Array of products
 $products = [
     [
-        'img' => '',
+        'img' => 'pictures/madarin.jpg',
         'alt' => 'Product 1',
         'name' => 'Product 1',
         'price' => 29.99
     ],
     [
-        'img' => '',
+        'img' => 'pictures/product2.jpg',
         'alt' => 'Product 2',
         'name' => 'Product 2',
         'price' => 39.99
     ],
     [
-        'img' => '',
+        'img' => 'pictures/product3.jpg',
         'alt' => 'Product 3',
         'name' => 'Product 3',
         'price' => 49.99
